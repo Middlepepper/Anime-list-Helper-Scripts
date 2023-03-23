f992858cf482eb3b4312175ef17a66986d0b3455
SomeGuy <Middlepepper@users.noreply.github.com>
2023-03-23 12:25:24 +1100

    Add files via upload
    A script to convert the .xml to .md to help with submitting pull request.
    
    This can be used on filed like https://github.com/Anime-Lists/anime-lists/blob/master/anime-list-unknown.xml

---
+++ XMLtoMarkdown.py
@@ -0,0 +1,51 @@
+import sys
+import xml.etree.ElementTree as ET
+
+# Check if file argument was provided
+if len(sys.argv) < 2:
+    print("Usage: python xml_to_markdown.py [file.xml]")
+    exit(1)
+
+# Parse XML file
+tree = ET.parse(sys.argv[1])
+root = tree.getroot()
+
+# Initialize Markdown table header
+table_header = "| AniDB | TVDB/TMDB/IMDB | Notes |\n|-------|----------------|-------|\n"
+
+# Initialize Markdown table rows
+table_rows = ""
+
+# Process each 'anime' element recursively
+def process_anime_element(element):
+    # Extract values from XML
+    anidbid = element.get("anidbid")
+    tvdbid = element.get("tvdbid")
+    name_element = element.find("name")
+    if name_element is not None:
+        name = name_element.text
+    else:
+        name = ""
+
+    # Format values into Markdown table row
+    row = f"| https://anidb.net/anime/{anidbid} | https://thetvdb.com/index.php?tab=series&id={tvdbid}| {name} |\n"
+
+    # Add row to table_rows
+    global table_rows
+    table_rows += row
+
+    # Recursively process child elements
+    for child in element:
+        if child.tag == "anime":
+            process_anime_element(child)
+
+# Start recursive processing with root element
+for child in root:
+    if child.tag == "anime":
+        process_anime_element(child)
+
+# Combine header and rows into Markdown table
+table = table_header + table_rows
+
+# Print Markdown table
+print(table)
